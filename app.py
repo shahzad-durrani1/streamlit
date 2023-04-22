e0d7e8b3df6a0a09522163721109cef3fb39f25b
shahzad-durrani1 <shahzad.durrani77@gmail.com>
2023-04-23 03:45:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,44 @@ df = pd.DataFrame(ws.get_all_records())
 
 
 # st.write(df.head(10))
+
+complaints_total = df['count of complaint_id'].sum()
+
+print(complaints_total)
+
+complaints_by_state = df.groupby('state')['count of complaint_id'].sum()
+
+print(complaints_by_state)
+
+
+# Total Number of Complaints with Closed Status
+closed_rows = df.loc[df['company_response'].str.contains('closed', case=False)]
+
+total = closed_rows['count of complaint_id'].sum()
+
+print(total)
+
+# % of Timely Responded Complaints
+
+timely_yes = df.loc[df['timely'] == 'Yes']
+complaints_sum_timely_yes = timely_yes['count of complaint_id'].sum()
+
+print(complaints_sum_timely_yes)
+
+print((complaints_sum_timely_yes / complaints_total) * 100)
+
+
+# Total Number of Complaints with In Progress Status
+
+response_in_progress = df.loc[df['company_response'] == 'In progress']
+
+complaints_sum_in_progress = response_in_progress['count of complaint_id'].sum()
+
+print(complaints_sum_in_progress)
+
+
+
+
 # Load data from a Google Sheets spreadsheet
 
 # sheet_key = sheet_url.split("/")[-2]
@@ -41,52 +79,72 @@ df = pd.DataFrame(ws.get_all_records())
 
 
 
-# Container 1: KPIs with State Filter
-with st.container():
-    # Add title and filter widgets
-    st.title("KPIs")
-    state_filter = st.selectbox("Select state", ["State A", "State B", "State C", "State D"])
+# # Container 1: KPIs with State Filter
+# with st.container():
+#     # Add title and filter widgets
+#     st.title("KPIs")
+#     state_filter = st.selectbox("Select state", ["State A", "State B", "State C", "State D"])
     
-    # Add KPI widgets with placeholder values
-    kpi1, kpi2, kpi3, kpi4 = st.beta_columns(4)
-    with kpi1:
-        st.subheader("KPI 1")
-        st.write("100")
-    with kpi2:
-        st.subheader("KPI 2")
-        st.write("200")
-    with kpi3:
-        st.subheader("KPI 3")
-        st.write("300")
-    with kpi4:
-        st.subheader("KPI 4")
-        st.write("400")
+#     # Add KPI widgets with placeholder values
+#     kpi1, kpi2, kpi3, kpi4 = st.beta_columns(4)
+#     with kpi1:
+#         st.subheader("KPI 1")
+#         st.write("100")
+#     with kpi2:
+#         st.subheader("KPI 2")
+#         st.write("200")
+#     with kpi3:
+#         st.subheader("KPI 3")
+#         st.write("300")
+#     with kpi4:
+#         st.subheader("KPI 4")
+#         st.write("400")
 
-# Container 2: Two charts side by side
-with st.container():
-    # Add title
-    st.title("Charts 1 and 2")
+# # Container 2: Two charts side by side
+# with st.container():
+#     # Add title
+#     st.title("Charts 1 and 2")
     
-    # Add two chart widgets side by side
-    chart1, chart2 = st.beta_columns(2)
-    with chart1:
-        st.subheader("Chart 1")
-        st.line_chart({"x": [1, 2, 3], "y": [10, 20, 30]})
-    with chart2:
-        st.subheader("Chart 2")
-        st.bar_chart({"x": [1, 2, 3], "y": [10, 20, 30]})
+#     # Add two chart widgets side by side
+#     chart1, chart2 = st.beta_columns(2)
+#     with chart1:
+#         st.subheader("Chart 1")
+#         st.line_chart({"x": [1, 2, 3], "y": [10, 20, 30]})
+#     with chart2:
+#         st.subheader("Chart 2")
+#         st.bar_chart({"x": [1, 2, 3], "y": [10, 20, 30]})
 
-# Container 3: Two charts side by side
-with st.container():
-    # Add title
-    st.title("Charts 3 and 4")
+# # Container 3: Two charts side by side
+# with st.container():
+#     # Add title
+#     st.title("Charts 3 and 4")
     
-    # Add two chart widgets side by side
-    chart3, chart4 = st.beta_columns(2)
-    with chart3:
-        st.subheader("Chart 3")
-        st.area_chart({"x": [1, 2, 3], "y": [10, 20, 30]})
-    with chart4:
-        st.subheader("Chart 4")
-        st.line_chart({"x": [1, 2, 3], "y": [10, 20, 30]})
+#     # Add two chart widgets side by side
+#     chart3, chart4 = st.beta_columns(2)
+#     with chart3:
+#         st.subheader("Chart 3")
+#         st.area_chart({"x": [1, 2, 3], "y": [10, 20, 30]})
+#     with chart4:
+#         st.subheader("Chart 4")
+#         st.line_chart({"x": [1, 2, 3], "y": [10, 20, 30]})
 
+# Define the sidebar options
+sidebar_options = st.sidebar.multiselect(
+    'Select State(s)',
+    sorted(df['state'].unique())
+)
+
+# Filter the DataFrame by state if the user selected any states in the sidebar
+if sidebar_options:
+    df = df.loc[df['state'].isin(sidebar_options)]
+
+# Display the total number of complaints and number of complaints per state
+st.write('### Summary')
+st.write('Total number of complaints:', len(df))
+st.write('Number of complaints per state:')
+st.write(df['state'].value_counts())
+
+# Display a bar chart of the number of complaints per state
+st.write('### Number of complaints per state')
+chart_data = df['state'].value_counts()
+st.bar_chart(chart_data)
