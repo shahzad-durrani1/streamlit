bb14a381112121d77a2e56e428a70f1a898187e7
shahzad-durrani1 <shahzad.durrani77@gmail.com>
2023-04-24 04:17:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -237,6 +237,9 @@ chart = alt.Chart(complaints_by_product).mark_bar().encode(
 
 # Display the chart in Streamlit
 
+product_counts = df.groupby('product')['count of complaint_id'].count().sort_values(ascending=False)
+
+
 
 
 # Container 2: Two charts side by side
@@ -247,7 +250,8 @@ with st.container():
     # Add two chart widgets side by side
     chart1, chart2 = st.columns([4,3])
     with chart1:
-        st.altair_chart(chart)
+        # st.altair_chart(chart)
+        st.bar_chart(product_counts)
     with chart2:
         st.subheader("Chart 2")
         st.bar_chart({"x": [1, 2, 3], "y": [10, 20, 30]})
